5bf11d9ffd63da98af7497fa1153f645a030cf26
avidrucker <avidrucker@gmail.com>
2024-04-18 11:40:07 -0400

    fix bug where width, height init were swapped

--- maptest1.c
+++ maptest1.c
@@ -3,7 +3,8 @@
 #include <string.h>
 #include <time.h>
 
-struct Rectangle {
+struct Rectangle
+{
     int xPos;
     int yPos;
     int width;
@@ -13,120 +14,185 @@ struct Rectangle {
 int ROWS = 30;
 int COLS = 60;
 
-void fillMatrix(char matrix[][COLS], int rows, int cols, char input) {
+void fillMatrix(char matrix[][COLS], int rows, int cols, char input)
+{
     int i, j;
 
-    for (i = 0; i < rows; i++) {
-        for (j = 0; j < cols; j++) {
+    for (i = 0; i < rows; i++)
+    {
+        for (j = 0; j < cols; j++)
+        {
             matrix[i][j] = input;
         }
     }
 }
 
-void printMatrix(char matrix[][COLS], int rows, int cols) {
+void printMatrix(char matrix[][COLS], int rows, int cols)
+{
     int i, j;
 
-    for (i = 0; i < rows; i++) {
-        for (j = 0; j < cols; j++) {
+    for (i = 0; i < rows; i++)
+    {
+        for (j = 0; j < cols; j++)
+        {
             printf("%c ", matrix[i][j]);
         }
         printf("\n");
     }
 }
 
-int moveInBounds(char matrix[][COLS], int playerPos[], char direction) {
-    if (direction == 'w' && playerPos[0] > 0) {
+int moveInBounds(char matrix[][COLS], int playerPos[], char direction)
+{
+    if (direction == 'w' && playerPos[0] > 0)
+    {
         return 1;
-    } else if (direction == 'a' && playerPos[1] > 0) {
+    }
+    else if (direction == 'a' && playerPos[1] > 0)
+    {
         return 1;
-    } else if (direction == 's' && playerPos[0] < ROWS - 1) {
+    }
+    else if (direction == 's' && playerPos[0] < ROWS - 1)
+    {
         return 1;
-    } else if (direction == 'd' && playerPos[1] < COLS - 1) {
+    }
+    else if (direction == 'd' && playerPos[1] < COLS - 1)
+    {
         return 1;
-    } else {
+    }
+    else
+    {
         return 0;
     }
 }
 
-int rectCollision(struct Rectangle a, struct Rectangle b) {
+int rectCollision(struct Rectangle a, struct Rectangle b)
+{
     if (a.xPos < b.xPos + b.width &&
         a.xPos + a.width > b.xPos &&
         a.yPos < b.yPos + b.height &&
-        a.yPos + a.height > b.yPos) {
+        a.yPos + a.height > b.yPos)
+    {
         return 1;
-    } else {
+    }
+    else
+    {
         return 0;
     }
 }
 
-void placeRoom(char matrix[][COLS], int x, int y, int width, int height, char wallChar) {
+void placeRoom(char matrix[][COLS], int x, int y, int width, int height, char wallChar)
+{
     int i, j;
 
-    for (i = x; i < x + width; i++) {
-        for (j = y; j < y + height; j++) {
-            if (i == x || i == x + width - 1 || j == y || j == y + height - 1) {
-                matrix[i][j] = wallChar;
-            } else {
-                matrix[i][j] = '.';
+    for (i = x; i < x + width; i++)
+    {
+        for (j = y; j < y + height; j++)
+        {
+            if (i == x || i == x + width - 1 || j == y || j == y + height - 1)
+            {
+                matrix[j][i] = wallChar;
+            }
+            else
+            {
+                matrix[j][i] = '.';
             }
         }
     }
 }
 
-void placeRooms(char matrix[][COLS], int numRooms) {
+// TIL: You cannot return an array from a function in C, but you can return a pointer to an array
+struct Rectangle *placeRooms(char matrix[][COLS], int numRooms)
+{
     int i, x, y, width, height;
 
     int placed = 0;
-    struct Rectangle rooms[9] = {};
-    while (placed < numRooms) {
-        width = rand() % 9 + 7;
+    int count = 0;
+    struct Rectangle *rooms = malloc(numRooms * sizeof(struct Rectangle));
+    while (placed < numRooms)
+    {
+        width = rand() % 10 + 7;
         height = rand() % 10 + 7;
-        x = rand() % (ROWS - width - 2) + 2; // min 2, max ROWS - width - 2 
-        y = rand() % (COLS - height - 2) + 2; // min 2, max COLS - height - 2
-        // printf("x: %d, y: %d, width: %d, height: %d\n", x, y, width, height);
+        x = rand() % (COLS - width - 2) + 2;  // min 2, max ROWS - height - 2
+        y = rand() % (ROWS - height - 2) + 2; // min 2, max COLS - width - 2
         struct Rectangle c = {x, y, width, height};
-        if(placed > 0) {
+        if (placed > 0)
+        {
             int collisionFound = 0;
-            for (i = 0; i < placed; i++) {
-                if (rectCollision(c, rooms[i])) {
+            for (i = 0; i < placed; i++)
+            {
+                if (rectCollision(c, rooms[i]))
+                {
                     collisionFound = 1;
                     break;
                 }
             }
-            
-            if(collisionFound) {
+
+            if (collisionFound)
+            {
                 // printf("collision found\n");
-            } else {
+            }
+            else
+            {
                 printf("placing room %d\n", (placed + 1));
-                placeRoom(matrix, x+1, y+1, width-2, height-2, i + '0');
+                printf("x: %d, y: %d, width: %d, height: %d\n", x, y, width, height);
+                placeRoom(matrix, x + 1, y + 1, width - 2, height - 2, i + '0');
                 rooms[placed] = c;
                 placed++;
             }
-        } else {
+        }
+        else
+        {
             printf("placing first room\n");
-            placeRoom(matrix, x+1, y+1, width-2, height-2, i + '0');
+            printf("x: %d, y: %d, width: %d, height: %d\n", x, y, width, height);
+            placeRoom(matrix, x + 1, y + 1, width - 2, height - 2, i + '0');
             rooms[placed] = c;
             placed++;
         }
+        count++;
+        if (count > 50)
+        {
+            // clear rooms & start over
+            placed = 0;
+            count = 0;
+            for (int i = 0; i < numRooms; i++)
+            {
+                struct Rectangle noRoom = {-1, -1, -1, -1};
+                rooms[i] = noRoom;
+            }
+            fillMatrix(matrix, ROWS, COLS, '-');
+        }
     }
+    return rooms;
 }
 
-int main() {
+int main()
+{
     srand(time(NULL));
-    char matrix[ROWS][COLS];
-    char input;
+    char matrix[ROWS][COLS]; // the board
+    char input; // character move input: 'wasd' or 'q'
+    // initialize display message that gives player info regarding out of bounds, etc.
     char message[80];
+    strcpy(message, "");
     // initialize player 1 at 0,0
+    // TODO: make playerLocation a Point struct
+    // TODO: set player's location to be in a room (make sure to prevent wall collisions)
+    // TODO: set player's location to be in the "first room" which may be farther away 
+    // from the level end, or simply just in room #0
     int playerLocation[2] = {0, 0};
-    while(1) {
+    // remember the tile type the player is on currently
+    char playerCell = '-';
+    // clear the board
+    fillMatrix(matrix, ROWS, COLS, '-');
+    // place rooms
+    struct Rectangle *rooms = placeRooms(matrix, 9);
+
+    // initially place player onto the board
+    matrix[playerLocation[0]][playerLocation[1]] = 'P';
+
+    while (1)
+    {
         // clear the console
         system("clear");
-        // clear the board
-        fillMatrix(matrix, ROWS, COLS, '-');
-        // update player location on the board
-        matrix[playerLocation[0]][playerLocation[1]] = 'P';
-        // place rooms
-        placeRooms(matrix, 9);
         // print message
         printf("%s\n", message);
         // print the board w/ player on it
@@ -134,41 +200,40 @@ int main() {
         // take in user input WASD to move player 1
         printf("Enter a direction to move (wasd) or q to quit: ");
         scanf(" %c", &input); // TIL: space before %c to skip whitespace, including newline
-        if(input == 'q') {
-            // strcpy(message, "Quitting...");
+        if (input == 'q')
+        {
             printf("Quitting...\n");
             break;
-        } else if (input == 'w') {
-            if(moveInBounds(matrix, playerLocation, input)) {
+        }
+        else // not quitting
+        {
+            if (moveInBounds(matrix, playerLocation, input)) {            
+                // Before updating the player's position, restore the previous cell
+                matrix[playerLocation[0]][playerLocation[1]] = playerCell;
+                // clear the message
                 strcpy(message, "");
-                playerLocation[0]--;
-            } else {
-                strcpy(message, "Invalid move");
-            }
-        } else if (input == 'a') {
-            if(moveInBounds(matrix, playerLocation, input)) {
-                strcpy(message, "");
-                playerLocation[1]--;
-            } else {
-                strcpy(message, "Invalid move");
-            }
-        } else if (input == 's') {
-            if(moveInBounds(matrix, playerLocation, input)) {
-                strcpy(message, "");
-                playerLocation[0]++;
-            } else {
-                strcpy(message, "Invalid move");
-            }
-        } else if (input == 'd') {
-            if(moveInBounds(matrix, playerLocation, input)) {
-                strcpy(message, "");
-                playerLocation[1]++;
+                // Update the player's position
+                if (input == 'w') {
+                    playerLocation[0]--;
+                } else if (input == 'a') {
+                    playerLocation[1]--;
+                } else if (input == 's') {
+                    playerLocation[0]++;
+                } else if (input == 'd') {;
+                    playerLocation[1]++;
+                } else {
+                    strcpy(message, "Error code 1: Invalid input");
+                }
+                // Store the original character of the new cell and place the player
+                playerCell = matrix[playerLocation[0]][playerLocation[1]];
+                matrix[playerLocation[0]][playerLocation[1]] = 'P';
             } else {
                 strcpy(message, "Invalid move");
             }
         }
     }
     printf("Thanks for playing!\n");
+    free(rooms);
 
     return 0;
 }
